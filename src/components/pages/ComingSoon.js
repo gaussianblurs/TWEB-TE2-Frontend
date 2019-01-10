9d451498d1776a5f85062fca8630011dd4be5cfe
GaussianBlurs <arnoldvbg@gmail.com>
2019-01-10 16:15:23 +0100

    rendu

--- src/components/pages/ComingSoon.js
+++ src/components/pages/ComingSoon.js
@@ -15,6 +15,7 @@ class ComingSoon extends React.Component {
     super(props)
     this.state = {
       movies: [],
+      genres: new Map(),
       page: 1,
       totalPages: 1
     }
@@ -38,6 +39,16 @@ class ComingSoon extends React.Component {
         this.fetchMovies()
       }
     })
+    .then(() => {
+      axios.get('http://api.themoviedb.org/3/genre/movie/list?api_key=f1be4bafe6f7cb0cb84f5948c5b75497')
+        .then((response) => {
+          response.data.genres.forEach((genre) => {
+            this.setState(prevState => ({
+              genres: prevState.genres.set(genre.id, genre.name)
+            }))
+          })
+        })
+    })
 
   render() {
     const { movies } = this.state
@@ -46,7 +57,7 @@ class ComingSoon extends React.Component {
         <Container>
           <Row>
             {
-              movies.map(movie => (
+              movies.filter(movie => movie.genre_ids.includes(28)).map(movie => (
                 <Card key={movie.id}>
                   <CardImg top height={200} width="auto" src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
                   <CardBody>
@@ -54,6 +65,12 @@ class ComingSoon extends React.Component {
                     <CardSubtitle>
                       Date de sortie:&nbsp;
                       { new Date(movie.release_date).toLocaleString('fr-ch') }
+                      <br />
+                      Genres:
+                      {' '}
+                      {
+                        movie.genre_ids.map(id => `${this.state.genres.get(id)} `)
+                      }
                     </CardSubtitle>
                   </CardBody>
                 </Card>
